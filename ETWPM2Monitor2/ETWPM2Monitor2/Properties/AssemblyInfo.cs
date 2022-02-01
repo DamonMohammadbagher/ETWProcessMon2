93a9b3ff1f8dd2c1cb100a0fa259433a29a2d4ef
Damon Mohammadbagher <DamonMohammadbagher@users.noreply.github.com>
2022-02-01 23:31:41 +0000

    Add files via upload

--- ETWPM2Monitor2/ETWPM2Monitor2/Properties/AssemblyInfo.cs
+++ ETWPM2Monitor2/ETWPM2Monitor2/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ETWPM2Monitor2")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Published by Damon Mohammadbagher , 2021")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("ETWPM2Monitor2")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
+[assembly: AssemblyCopyright("Published by Damon Mohammadbagher , Copyright © Microsoft 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.78.6")]
-[assembly: AssemblyFileVersion("2.0.78.6")]
+[assembly: AssemblyVersion("2.1.0.87")]
+[assembly: AssemblyFileVersion("2.1.0.87")]
