56142935c409413d8de8c66b72c8d9895a6e7682
Damon Mohammadbagher <DamonMohammadbagher@users.noreply.github.com>
2022-02-13 15:43:47 +0000

    Add files via upload

--- ETWPM2Monitor2/ETWPM2Monitor2/Properties/AssemblyInfo.cs
+++ ETWPM2Monitor2/ETWPM2Monitor2/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.25.50")]
-[assembly: AssemblyFileVersion("2.1.25.50")]
+[assembly: AssemblyVersion("2.1.12.37")]
+[assembly: AssemblyFileVersion("2.1.12.37")]
