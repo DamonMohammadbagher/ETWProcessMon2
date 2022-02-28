fa6cd4ae4c0aa526a0ef5b7246a98505740d99c6
Damon Mohammadbagher <DamonMohammadbagher@users.noreply.github.com>
2022-02-28 20:47:12 +0000

    Add files via upload

---
+++ SysPM2Monitor2.7/SysPM2Monitor2_7/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SysPM2Monitor2_7")]
+[assembly: AssemblyDescription("Published by Damon Mohammadbagher 10 Jan 2022")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SysPM2Monitor2_7")]
+[assembly: AssemblyCopyright("Published by Damon Mohammadbagher 10 Jan 2022, Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a77a210c-71d8-4eed-b201-47ea52be2eab")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.7.12.58")]
+[assembly: AssemblyFileVersion("2.7.12.58")]
