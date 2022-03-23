69c9bb65db44eb42655562e1818008f5f97c77cc
Damon Mohammadbagher <DamonMohammadbagher@users.noreply.github.com>
2022-03-23 22:53:04 +0000

    Add files via upload

---
+++ VirtualMemAllocMon/VirtualMemAllocMon2/VirtualMemAllocMon/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Diagnostics.Tracing.Session;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using System.Windows.Forms;
+
+namespace VirtualMemAllocMon
+{
+
+    class Program
+    {
+
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+
+            }
+            catch (Exception ee)
+            {
+              
+            }
+
+
+        }
+      
+    }
+}
